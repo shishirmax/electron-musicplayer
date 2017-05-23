3e302da67e4ce21532314f50d196a404c9b60c17
shishirmax <shishirmax@gmail.com>
2017-05-23 09:50:59 +0530

    code update

--- app/views/player/player.js
+++ app/views/player/player.js
@@ -8,17 +8,94 @@
  }])
 
  .controller('PlayerCtrl',['$scope','$location',function($scope,$location){
-     $scope.musicSelected = false;
+     $scope.musicSelected = false,$scope.trackName = null;
+     $scope.songList = [];
      const ipc = require('electron').ipcRenderer;
      ipc.on('modal-file-content',function(event,arg){
          console.log(arg);
-         $scope.song = new Howl({
-             src:[arg]
-         });
+         $scope.songList = arg;
+         var songsArrayForPlaying = [];
+         for(var i =0;i<$scope.songList.length;i++){
+             songsArrayForPlaying.push({
+                 title:arg.path+'/'+$scope.songList[i],
+                 file:arg.path+'/'+$scope.songList[i],
+                 howl:null,
+                 name:$scope.songList[i]
+             })
+         }
+
+        //  $scope.song = new Howl({
+        //      src:[arg]
+        //  });
+        $scope.player = new Player(songsArrayForPlaying);
          $scope.musicSelected = true;
          $scope.$apply();
      })
      $scope.playMusic = function(){
-        $scope.song.play();
+        $scope.player.play();
+     }
+     var Player = function(playlist){
+         this.playlist = playlist;
+         this.index = 0;
+     }
+
+     Player.prototype = {
+         play:function(index){
+             var self = this;
+             var sound = null;
+             index = typeof index === 'number'?index:self.index;
+             var data = self.playlist[index];
+             $scope.trackName = data.name;
+             if(data.howl){
+                 sound = data.howl;
+             }
+             else{
+                 sound = data.howl = new Howl({
+                     src:[data.file],
+                     html5:true
+                 })
+             }
+             sound.play();
+             self.index = index;
+         },
+         pause:function(){
+             var self = this;
+             var sound = self.playlist[self.index].howl;
+             sound.pause();
+         },
+         skip:function(direction){
+             var self = this;
+             var index = 0;
+             if(direction === 'prev')
+             {
+                 index = self.index - 1 ;
+                 if(index<0)
+                 {
+                     index = self.playlist.length - 1;
+                 }
+             }
+             else{
+                 index = self.index + 1;
+                 if(index >= self.playlist.length)
+                 {
+                     index = 0;
+                 }
+             }
+             self.skipTo(index);
+         },
+         skipTo:function(index){
+             var self = this;
+             if(self.playlist[self.index].howl){
+                 self.playlist[self.index].howl.stop()
+             }
+             self.play(index);
+         },
+         seek:function(time){
+             var self = this;
+             var sound = self.playlist[self.index].howl;
+             if(sound.playing()){
+                 sound.seek(sound.duration()*time);
+             }
+         }
      }
  }])
--- main.js
+++ main.js
@@ -10,7 +10,7 @@ app.on('ready',function(){
         width:612,
         height:384
     });
-    //mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
 
     var template = [
         {
@@ -33,9 +33,20 @@ app.on('ready',function(){
 
 function openFolderDialog(){
 dialog.showOpenDialog(mainWindow,{
-    properties:['openFile']
-},function(filepath){
-    mainWindow.webContents.send('modal-file-content',filepath[0]);
-    console.log(filepath[0]);
+    properties:['openDirectory']
+},function(filePath){
+    fs.readdir(filePath[0],function(err,files){
+        var arr = [];
+        for(var i=0;i<files.length;i++){
+            if(files[i].substr(-4) === '.mp3'){
+                arr.push(files[i]);
+            }
+        }
+        console.log(arr);
+        var objToSend = {};
+        objToSend.files = arr;
+        objToSend.path = filePath[0];
+        mainWindow.webContents.send('modal-file-content',objToSend);
+    })
 })
 }
